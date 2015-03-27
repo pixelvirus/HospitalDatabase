6665a6ed98a14db807028843a73573516ad60bfd
David Song <q3t8@cugrad.cs.ubc.ca>
2015-03-26 22:13:03 -0700

    auto-format

--- hospital.sql
+++ hospital.sql
@@ -31,14 +31,14 @@ create table Patients
 	province varchar(2),
 	zip varchar(7),
 	insurance varchar(20)
-  );		
+);
 
 create table Doctors
 ( 
 	do_id int not null primary key,
 	do_name varchar(20),
 	specialization varchar(20) 
-  );
+);
 
 create table Bookings
 (
@@ -47,7 +47,7 @@ create table Bookings
 	bk_enddate date,
 	constraint bookingdate
 	check(bk_startdate<bk_enddate)
-	);
+);
 
 create table OperatingRooms
 ( 
@@ -76,7 +76,7 @@ create table RecoveryRoomBeds
 	recoveryroombed_bedNo int not null,
 	primary key (recoveryroom_id,recoveryroombed_bedNo),
 	foreign key (recoveryroom_id) references RecoveryRooms on delete cascade	
-  ); 
+);
  
 create table BookingReservesRecoveryRooms
 (
@@ -94,7 +94,7 @@ create table Procedures
 	proc_name varchar(20),
 	description varchar(40),
 	cost float 
-  );  
+);
  
 create table BookingForProcedures
 (
@@ -106,8 +106,6 @@ create table BookingForProcedures
 	foreign key (proc_id) references Procedures,
 	foreign key (do_id) references Doctors
   );
-  
-
 
  create table AdmittedTo
 ( 
@@ -117,14 +115,14 @@ create table BookingForProcedures
 	primary key (pt_id, recoveryroom_id, recoveryroombed_bedNo),
 	foreign key (pt_id) references Patients,
 	foreign key (recoveryroom_id,recoveryroombed_bedNo) references RecoveryRoomBeds(recoveryroom_id,recoveryroombed_bedNo)
-	);
+);
 	
   create table Medications
 ( 
 	med_id integer not null primary key,
 	med_name varchar(20),
 	cost float 
-  );
+);
   
 create table Prescribes
 ( 
@@ -137,7 +135,7 @@ create table Prescribes
 	foreign key (pt_id) references Patients,
 	foreign key (do_id) references Doctors,
 	foreign key (med_id) references Medications 
-  );
+);
 
 create table Performs
 ( 
@@ -151,13 +149,13 @@ create table Performs
 	foreign key (do_id) references Doctors,
 	foreign key (oproom_id) references OperatingRooms,
 	foreign key (proc_id) references Procedures 
-  );
+);
 
 create table Offices
 ( 
 	office_id integer not null primary key,
 	sqFoot int
-  );
+);
   
 create table HasAOffice
 ( 
@@ -166,19 +164,19 @@ create table HasAOffice
 	primary key (do_id, office_id),
 	foreign key (do_id) references Doctors on delete cascade,
 	foreign key (office_id) references Offices on delete cascade
-  );
+);
 
 create table StaffUsers
 (
 	userName varchar(20) primary key,
 	password varchar(20)
-  );
+);
   
 create table PatientUsers
 (
 	userName varchar(20) primary key,
 	password varchar(20)
-  );
+);
  
 create table SUserHasA
 (
@@ -187,7 +185,7 @@ create table SUserHasA
 	primary key (do_id, userName),
 	foreign key (do_id) references Doctors,
 	foreign key (userName) references StaffUsers
-  );  
+);
   
 create table PUserHasA
 (
@@ -196,7 +194,7 @@ create table PUserHasA
 	primary key (pt_id, userName),
 	foreign key (pt_id) references Patients,
 	foreign key (userName) references PatientUsers
-  );
+);
 
 /*Patients*/
 insert into patients
@@ -509,8 +507,6 @@ values('97640','876','40404','4000',TO_DATE('02-12-2015','DD-MM-YYYY'));
 
 insert into performs
 values('56841','555','20202','2000',TO_DATE('12-01-2015','DD-MM-YYYY'));
-
-
 
 /*Offices*/
 insert into offices
